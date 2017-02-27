fa1a4690bd3c5be9a747e806381f105d8af6e9e2
Вадим Стецюк < >
2017-02-27 13:22:43 +0200

    fix Ingredient class

--- basic4/Ingredient.php
+++ basic4/Ingredient.php
@@ -4,29 +4,24 @@ class Ingredient {
     private $name;
     private $cost;
 
-    public function __construct() {
-		$this->name = '';
-		$this->cost = 0.0;
-	}
-
 	public function __construct(string $name, float $cost) {
 		$this->name = $name;
 		$this->cost = $cost;
 	}
 
     public function getName() : string {
-        return $name;
+        return $this->name;
     }
 
-    public function setName(string $name) {
+    public function setName($name) {
         $this->name = $name;
     } 
 
-    public function getCost() : float{
-        return $cost;
+    public function getCost() : float {
+        return $this->cost;
     }   
 
-    public function setCost(float $cost) {
+    public function setCost($cost) {
         $this->cost = $cost;
     }
 }
